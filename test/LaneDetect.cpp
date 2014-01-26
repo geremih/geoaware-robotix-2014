cd2d04077db2a2e962b8aa9c71895ee553cd3d33
Mihir Rege <mihirrege@gmail.com>
2014-01-26 18:27:58 +0530

    Add lanedetect code

--- test/LaneDetect.cpp
+++ test/LaneDetect.cpp
@@ -1,48 +1,9 @@
-/*------------------------------------------------------------------------------------------*\
-  Lane Detection
-
-  General idea and some code modified from:
-  chapter 7 of Computer Vision Programming using the OpenCV Library. 
-  by Robert Laganiere, Packt Publishing, 2011.
-
-  This program is free software; permission is hereby granted to use, copy, modify, 
-  and distribute this source code, or portions thereof, for any purpose, without fee, 
-  subject to the restriction that the copyright notice may not be removed 
-  or altered from any source or altered source distribution. 
-  The software is released on an as-is basis and without any warranties of any kind. 
-  In particular, the software is not guaranteed to be fault-tolerant or free from failure. 
-  The author disclaims all warranties with regard to this software, any use, 
-  and any consequent failure, is purely the responsibility of the user.
- 
-  Copyright (C) 2013 Jason Dorweiler, www.transistor.io
-
-
-  Notes: 
-
-	Add up number on lines that are found within a threshold of a given rho,theta and 
-	use that to determine a score.  Only lines with a good enough score are kept. 
-
-	Calculation for the distance of the car from the center.  This should also determine
-	if the road in turning.  We might not want to be in the center of the road for a turn. 
-	
-	Several other parameters can be played with: min vote on houghp, line distance and gap.  Some
-	type of feed back loop might be good to self tune these parameters. 
-
-	We are still finding the Road, i.e. both left and right lanes.  we Need to set it up to find the
-	yellow divider line in the middle. 
-
-	Added filter on theta angle to reduce horizontal and vertical lines. 
-
-	Added image ROI to reduce false lines from things like trees/powerlines
-  \*------------------------------------------------------------------------------------------*/
-
 #include "opencv2/highgui/highgui.hpp"
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <vector>
 #include <stdio.h>
-#include "linefinder.h"
 
 #define PI 3.1415926
 
@@ -241,6 +202,8 @@ int cannyHigher = 250;
 Mat image;
 string argp;
 VideoCapture capture;
+
+
 
 
 void removeSymbols(Mat& img){
