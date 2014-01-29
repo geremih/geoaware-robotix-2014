a82c052416031a4df8bb0f7c5648ac9e8fc71016
Mihir Rege <mihirrege@gmail.com>
2014-01-29 23:52:02 +0530

    Camcontroller class

---
+++ src/CamController.h
@@ -0,0 +1,27 @@
+#ifndef CAMCONTROLLER_H
+#define CAMCONTROLLER_H
+#include "GeoAware.h"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+#define PI 3.1415926
+
+class CamController{
+
+ public:
+  string laneFollowDir(vector<cv::Mat> frames); // return direction
+  void isPassage(bool& pLeft,bool& pRight);
+
+  
+
+
+
+};
+
+
+#endif
